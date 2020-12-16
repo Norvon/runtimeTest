5d89b226eb98de8a1615d695f26914cb89bf8902
nor <norvon@126.com>
2020-12-16 18:06:52 +0800

    update

--- macOSRuntimeTest/main.m
+++ macOSRuntimeTest/main.m
@@ -7,13 +7,58 @@
 
 #import <Foundation/Foundation.h>
 
+#import <objc/runtime.h>
+#import <malloc/malloc.h>
+
+
+
+@interface Person : NSObject
+{
+    int _age;
+}
+@end
+@implementation Person
+@end
+
+@interface Student : Person
+{
+    int _no;
+}
+@end
+@implementation Student
+@end
+
 int main(int argc, const char * argv[]) {
     
-    NSString *s = [[NSString alloc] init];
     
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    NSLog(@"%zd %zd  %zd",
+          class_getInstanceSize([NSObject class]),
+          class_getInstanceSize([Person class]),
+          class_getInstanceSize([Student class])
+          );
+    NSObject *obj = [[NSObject alloc] init];
+    
+    malloc_size((__bridge const void *)(obj));
+    
+    
+    
+//    @autoreleasepool {
+//        // insert code here...
+//        NSLog(@"Hello, World!");
+//    }
+    
+//    Class objectMetaClass = object_getClass([NSObject class]);
+//    Class cls = [[NSObject class] class];
+//    Class objectClass3 = [NSObject class];
+//    class_isMetaClass(objectMetaClass);
+//
+//    NSLog(@"%p %p %p", objectMetaClass, objectClass3, cls); // 后面两个地址相同，说明多次调用class得到的还是类对象
+
+    NSObject *object = [[NSObject alloc] init];
+    Class objectClass = [NSObject class];
+    Class objectMetaClass = object_getClass([NSObject class]);
+    NSLog(@"%p %p %p", object, objectClass, objectMetaClass);
+    
+    
     return 0;
 }
